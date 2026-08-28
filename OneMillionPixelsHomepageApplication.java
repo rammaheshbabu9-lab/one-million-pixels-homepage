5625c4b33955cf1e982252f5df92871555be51fb
rammaheshbabu9-lab <rammaheshbabu9@gmail.com>
2026-08-28 15:21:25 +0530

    Initialize One Million Pixels Homepage application

---
+++ OneMillionPixelsHomepageApplication.java
@@ -0,0 +1,17 @@
+
+package com.ompindia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneMillionPixelsHomepageApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                OneMillionPixelsHomepageApplication.class,
+                args
+        );
+    }
+}
